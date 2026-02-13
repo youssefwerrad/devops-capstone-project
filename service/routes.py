62c34d2a46ce579676b3d9e561b5e66cf6e4c877
youssefwerrad <youssefwerrad@gmail.com>
2026-02-13 06:24:46 -0500

    Fix lint spacing issues

--- service/routes.py
+++ service/routes.py
@@ -91,10 +91,13 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")    
-    # return the serialize() version of the account with a return code of status.HTTP_200_OK
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.")
+    # return the serialize() version of the account with a return code of
+    # status.HTTP_200_OK
     return account.serialize(), status.HTTP_200_OK
-    
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -109,7 +112,9 @@ def update_accounts(account_id):
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
 
