94670ec1509eb289af8250fb0547b7dda28180be
youssefwerrad <youssefwerrad@gmail.com>
2026-02-14 12:13:35 -0500

    fixed flake8 linting errors - added required blank lines

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -170,6 +170,7 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
+
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -183,6 +184,7 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
+
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -190,3 +192,4 @@ class TestAccountService(TestCase):
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
         
+
